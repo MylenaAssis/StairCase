bf972d819f62b0c8586e40482dc98e9bd50ebb58
Mylena Assis <lena.assis.dev@gmail.com>
2024-11-06 16:23:52 -0300

    StairCase

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+
+//Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+//Write a program that prints a staircase of size n.
+
+
+class Result
+{
+    public static void staircase(int n)
+    {
+        String e = " ";
+        String s = "#";
+        int d = 1;
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            x = n - d;
+            y = d;
+            while (x > 0)
+            {
+                Console.Write(e);
+                x--;
+            }
+            while (y > 0)
+            {
+                Console.Write(s);
+                y--;
+            }
+            d++;
+            Console.WriteLine();
+        }
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
